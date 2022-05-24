b93dfbcc76c19ee4c1d0a78d4e1c19791d7f4833
Andrey Trusov <trusov@haulmont.com>
2022-05-25 01:30:24 +0400

    IOC - 2 step

--- src/main/java/com/example/spring/ApplicationRunner.java
+++ src/main/java/com/example/spring/ApplicationRunner.java
@@ -3,16 +3,25 @@ package com.example.spring;
 import com.example.spring.db.pool.ConnectionPool;
 import com.example.spring.db.repository.CompanyRepository;
 import com.example.spring.db.repository.UserRepository;
+import com.example.spring.ioc.Container;
 import com.example.spring.service.UserService;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var connectionPool = new ConnectionPool();
-        var userRepository = new UserRepository(connectionPool);
-        var companyRepository = new CompanyRepository();
 
-        var userService = new UserService(userRepository, companyRepository);
+        var container = new Container();
+
+        var connectionPool = container.get(ConnectionPool.class);
+        var userRepository = container.get(UserRepository.class);
+        var companyRepository = container.get(CompanyRepository.class);
+        var userService = container.get(UserService.class);
+
+
+//        var connectionPool = new ConnectionPool();
+//        var userRepository = new UserRepository(connectionPool);
+//        var companyRepository = new CompanyRepository();
+//        var userService = new UserService(userRepository, companyRepository);
         //TODO userService
 
 
